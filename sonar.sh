e9cf779605a57de75b5b60cc5344428e33e1e8ef
SibaDoge <rladus753159@gmail.com>
2018-11-29 04:12:23 +0900

    gradle 수정

--- sonar.sh
+++ sonar.sh
@@ -1,1 +1,2 @@
+./gradlew build connectedCheck
 ./gradlew sonarqube -Dsonar.projectKey=SibaDoge1_CAU-Hanja -Dsonar.organization=sibadoge1-github -Dsonar.host.url=https://sonarcloud.io -Dsonar.login=$SONAR_TOKEN
